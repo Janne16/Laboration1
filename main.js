32dd161b2754d394081149da717604bba0e9a1ae
Johan Rääf <johanraaf@hotmail.com>
2022-11-14 16:31:23 +0100

    added jumpOrStay question

--- main.js
+++ main.js
@@ -2,14 +2,17 @@
 window.main('DOMContentLoaded', main);
 
 
-// FRÅGA DAVID
+// FRÅGA DAVID hur det funkar med main/chooseSkisOrSnowboard
+// FRÅGA DAVID om debugen, funkar ej
 
 function main() {
     const introductionOfAdventureAlert = alert("Välkommen till Johans snöiga äventyr!")
     const descriptionOfAdventureAlert = alert("Vi befinner oss i st. Anton. Det är mitt i vintern, -10 och strolande sol.")
     const firstQuestionOfAdventureAlert = alert("Vi ska börja med att ta oss upp i liften och här kommer det första, och absolut viktigaste, valet, skidor eller snowboard:")
     chooseSkisOrSnowboard();
+    chooseJumpOrStay();
     // call other function in future....
+    
 }
 
 function chooseSkisOrSnowboard() {
@@ -32,12 +35,42 @@ function chooseSkisOrSnowboard() {
             chooseSkisOrSnowboard();
         }
     }
-    
+
     else {
         alert("Testa igen! Någonting blev fel");
         chooseSkisOrSnowboard();
     }
 }
+
+function chooseJumpOrStay() {
+    let questionJumpOrStay = prompt("'hoppa' eller 'sitta kvar");
+    if (questionJumpOrStay == 'hoppa') {
+        alert("Ajdå, du dog.");
+    }
+    else if (questionJumpOrStay == "sitt kvar") {
+        alert("Liften börjar rulla efter 5 minuter");
+    }
+    else {
+        alert("Testa igen! Någonting blev fel");
+        chooseJumpOrStay();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
