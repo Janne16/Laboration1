2d55706d58df6d1efda095df33494b4e05bae844
Johan Rääf <johanraaf@hotmail.com>
2022-11-14 14:04:01 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Johan Rääfs snöiga äventyr
+
+I denna uppgift ska användaren få följa med på ett snöigt äventyr med oförutsägbara svängar.
---
+++ main.js
@@ -0,0 +1,5 @@
+let skisOrSnowboard = prompt("Skriv 'skidor eller 'snowboard'");
+function skisOrSnowboard() {
+    if ("skidor" === skidor)
+    
+}
