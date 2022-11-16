19cf490ca4d924d3926b6b1e62337e34e02b2d0b
Johan Rääf <johanraaf@hotmail.com>
2022-11-16 16:21:36 +0100

    commenting everything out to start over

--- main.js
+++ main.js
@@ -1,60 +1,73 @@
-// window.chooseSkisOrSnowboard('DOMContentLoaded', main);
-window.main('DOMContentLoaded', main);
+// window.addEventListener('DOMContentLoaded', main);
 
+// Hur funkar PUSH BRANSCH/SYNC
 
-// FRÅGA DAVID hur det funkar med main/chooseSkisOrSnowboard
-// FRÅGA DAVID om debugen, funkar ej
-
-function main() {
-    const introductionOfAdventureAlert = alert("Välkommen till Johans snöiga äventyr!")
-    const descriptionOfAdventureAlert = alert("Vi befinner oss i st. Anton. Det är mitt i vintern, -10 och strolande sol.")
-    const firstQuestionOfAdventureAlert = alert("Vi ska börja med att ta oss upp i liften och här kommer det första, och absolut viktigaste, valet, skidor eller snowboard:")
-    chooseSkisOrSnowboard();
-    chooseJumpOrStay();
+// function main() {
+//     const introductionOfAdventureAlert = alert("Välkommen till Johans snöiga äventyr!")
+//     const descriptionOfAdventureAlert = alert("Vi befinner oss i st. Anton. Det är mitt i vintern, -10 och strolande sol.")
+//     const firstQuestionOfAdventureAlert = alert("Vi ska börja med att ta oss upp i liften och här kommer det första och absolut viktigaste valet, skidor eller snowboard:")
+//     chooseSkisOrSnowboard();
+//     chooseJumpOrStay();
     // call other function in future....
     
-}
+// }
 
-function chooseSkisOrSnowboard() {
+// function chooseSkisOrSnowboard() {
 
-    const questionSkisOrSnowboard = prompt("Skriv 'skidor' eller 'snowboard' (eller 'börja om' för att börja)");
+//     const questionSkisOrSnowboard = prompt("Skriv 'skidor' eller 'snowboard' (eller 'börja om' för att börja)");
 
-    if (questionSkisOrSnowboard == "skidor") {
-        alert("Rackars! Skidorna har inte hunnit vallas än... Du får tyvärr välja snowboard. Tryck 'ok' för att välja snowboard");
-    }
-    else if (questionSkisOrSnowboard == "snowboard") {
-        alert("Bra val! Nu ska vi ta oss upp i liften");
-    }
-    else if (questionSkisOrSnowboard == "börja om") {
-        const confirmationQuestion = prompt("är du säkert? 'ja' eller 'nej'");
-        if (confirmationQuestion == "ja") {
-            main();
-        }
-        else if (confirmationQuestion == "nej") {
-            alert("okej då väljer vi skidor eller snowboard!");
-            chooseSkisOrSnowboard();
-        }
-    }
+//     if (questionSkisOrSnowboard == "skidor") {
+//         alert("Rackars! Skidorna har inte hunnit vallas än... Du får tyvärr välja snowboard. Tryck 'ok' för att välja snowboard");
+//         alert("Bra val! Nu ska vi ta oss upp i liften");
+//         chooseJumpOrStay();
 
-    else {
-        alert("Testa igen! Någonting blev fel");
-        chooseSkisOrSnowboard();
-    }
-}
+//     }
+//     else if (questionSkisOrSnowboard == "snowboard") {
+//         alert("Bra val! Nu ska vi ta oss upp i liften");
+//         chooseJumpOrStay();
+//     }
+    // else if (questionSkisOrSnowboard == "börja om") {
+    //     const confirmationQuestion = prompt("är du säkert? 'ja' eller 'nej'");
+    //     if (confirmationQuestion == "ja") {
+    //         main();
+    //     }
+    //     else if (confirmationQuestion == "nej") {
+    //         alert("okej då väljer vi skidor eller snowboard!");
+    //         chooseSkisOrSnowboard();
+    //     }
+    // }
+//     else {
+//         alert("Testa igen! Någonting blev fel");
+//         chooseSkisOrSnowboard();
+//     }
 
-function chooseJumpOrStay() {
-    let questionJumpOrStay = prompt("'hoppa' eller 'sitta kvar");
-    if (questionJumpOrStay == 'hoppa') {
-        alert("Ajdå, du dog.");
-    }
-    else if (questionJumpOrStay == "sitt kvar") {
-        alert("Liften börjar rulla efter 5 minuter");
-    }
-    else {
-        alert("Testa igen! Någonting blev fel");
-        chooseJumpOrStay();
-    }
-}
+//     checkIfRestart(questionSkisOrSnowboard)
+//     alert("okej då väljer vi skidor eller snowboard!");
+//     chooseSkisOrSnowboard();
+// }
+
+// function checkIfRestart(value) {
+//     if (value == "börja om") {
+//         const confirmationQuestion = prompt("är du säkert? 'ja' eller 'nej'");
+//         if (confirmationQuestion == "ja") {
+//             main();
+//         }
+//     }
+// }
+
+// function chooseJumpOrStay() {
+//     let questionJumpOrStay = prompt("'hoppa' eller 'sitta kvar");
+//     if (questionJumpOrStay == 'hoppa') {
+//         alert("Ajdå, du dog.");
+//     }
+//     else if (questionJumpOrStay == "sitt kvar") {
+//         alert("Liften börjar rulla efter 5 minuter");
+//     }
+//     else {
+//         alert("Testa igen! Någonting blev fel");
+//         chooseJumpOrStay();
+//     }
+// }
 
 
 
@@ -92,3 +105,9 @@ function chooseJumpOrStay() {
 //   const header = document.querySelector("header");
 //   header.classList.toggle("open");
 // }
+
+window.addEventListener('DOMContentLoaded', main);
+
+function main() {
+    
+}
